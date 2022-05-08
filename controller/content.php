b3f11779f53479cc1ec5ed3fb6c27cb214e645a9
Donatella7 <campaniellodonatella@gmail.com>
2022-05-08 15:42:50 +0200

    style

--- controller/content.php
+++ controller/content.php
@@ -28,14 +28,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="../style/partials/content.css">
     <link rel="stylesheet" href="../style/common.css">
     <title>Document</title>
 </head>
 <body>
     <main>
-    <section class="wrapper-cards-movie p-3">
+        <section class="wrapper-cards-movie p-3">
             <div class="row justify-content-center row-cards-movie">
-
                 <div class="card card-content col-2">
                     <div class="d-flex justify-content-center p-2">
                         <img class="card-img-top" src="<?php  echo $content_created->cover ?>" alt="Card image content">
@@ -43,26 +43,20 @@
                     <div class="card-body card-content-body">
                         <h5 class="card-title">Title:</h5>
                         <p><?php echo $content_created->title ?></p>
-
                         <h5>Director:</h5>
                         <p><?php echo $content_created->director ?></p>
-
                         <h5>Year:</h5>
                         <p><?php echo $content_created->year ?></p>
                         <h5>Type content: </h5>
                         <p><?php echo $content_created->type_content ?></p>
                     </div>
-
                     <div class="card-body comment-body">
                         <h5 class="card-title">Username:</h5>
                         <p><?php echo $comment_created->username ?></p>
-
                         <h5>Vote:</h5>
                         <p><?php echo $comment_created->vote ?></p>
-
                         <h5>Comment:</h5>
                         <p><?php echo $comment_created->comment ?></p>
-                
                     </div>
                 </div>
             </div>
--- view/index.php
+++ view/index.php
@@ -10,6 +10,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="../style/partials/index.css">
     <link rel="stylesheet" href="../style/common.css">
     <title>Stream</title>
 </head>
@@ -22,7 +23,7 @@
                         <!-- content  -->
                         <h1 class="mb-2">Insert your content</h1>
                         <div class="box-input-title d-flex justify-content-between mb-2">
-                            <label for="input-title">Inserisi il titolo del contenuto</label>
+                            <label for="input-title">Insert title</label>
                             <input id="input-title" type="text" name="title" placeholder="contenuto">
                         </div>
                         <div class="box-input-year d-flex justify-content-between mb-2">
